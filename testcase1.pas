2cbca69d967feade75cd594bc73648a1e73b86ef
LazyOnPascal <https://github.com/LazyOnPascal>
2023-01-16 07:46:01 +0400

    Add test

---
+++ testcase1.pas
@@ -0,0 +1,54 @@
+unit TestCase1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fpcunit, testregistry;
+
+const
+  DEFAULT_BRIDGE_1 = '';
+  DEFAULT_BRIDGE_2 = '';
+  CONNECTION_TIMEOUT = 100;
+
+type
+
+  TTorTest= class(TTestCase)
+  published
+    procedure TorConnect;
+  end;
+
+implementation
+
+uses
+  TorLauncher;
+
+procedure TTorTest.TorConnect;
+var
+  tor: TTorLauncher;
+  vTimer: byte;
+begin
+  tor := TTorLauncher.Create('', 'TorWorkDir',
+    9151, 9152, TtorBridges.Create(DEFAULT_BRIDGE_1, DEFAULT_BRIDGE_2, ''));
+  try
+    tor.process.Execute;
+    for vTimer := 0 to CONNECTION_TIMEOUT do
+    begin
+      Write(tor.GetNewOutput);
+      Sleep(1000);
+      if tor.ready or tor.error then break;
+    end;
+    AssertTrue('Tor not connected!', tor.ready);
+  finally
+    tor.Free;
+  end;
+end;
+
+
+
+initialization
+
+  RegisterTest(TTorTest);
+end.
+
